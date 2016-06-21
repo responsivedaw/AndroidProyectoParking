5ef7462385b874bd2f7ef03ace87043689cf5a68
manana <manana@manana.es>
2016-06-21 13:35:34 +0200

    AÑADIDA LIBRERIA KSOAP2

--- app/src/main/java/apptizer/es/androidproyectoparking/model/Utility4.java
+++ app/src/main/java/apptizer/es/androidproyectoparking/model/Utility4.java
@@ -1,5 +1,7 @@
 package apptizer.es.androidproyectoparking.model;
 
+import org.ksoap2.serialization.SoapObject;
+
 /**
  * Created by DavidRN on 20/6/16.
  */
@@ -7,5 +9,6 @@ package apptizer.es.androidproyectoparking.model;
 public class Utility4 {
 
     // Comentario en Utility 4 desde clase
+    SoapObject request;
 
 }
